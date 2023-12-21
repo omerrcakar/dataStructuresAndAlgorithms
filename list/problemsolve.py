fee4411b9f1804945e10e002800fa974915ef936
omer cakar <omerrccak@gmail.com>
2023-12-21 23:44:58 +0300

    added comment line for problem3

--- list/problemsolve.py
+++ list/problemsolve.py
@@ -61,4 +61,10 @@ print(sonuc)
           
 
 
+"""
+Problem3: Given an array nums of size n, return the majority element.
 
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+"""
